da98b4e06c35df38162ecc41e64f81681f182d94
HarryPottahr <77153358+HarryPottahr@users.noreply.github.com>
2021-01-30 10:51:05 +0530

    Update afk_sql.py

--- SaitamaRobot/modules/sql/afk_sql.py
+++ SaitamaRobot/modules/sql/afk_sql.py
@@ -1,101 +1,191 @@
-import threading
+import random
+import time
 
-from SaitamaRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, UnicodeText
+from YuiiChan import dispatcher
+from YuiiChan.modules.disable import DisableAbleCommandHandler
+from YuiiChan.modules.sql import afk_sql as sql
+from YuiiChan.modules.users import get_user_id
+from telegram import MessageEntity, Update
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
 
-
-class AFK(BASE):
-    __tablename__ = "afk_users"
-
-    user_id = Column(Integer, primary_key=True)
-    is_afk = Column(Boolean)
-    reason = Column(UnicodeText)
-    afk_time = Column(Integer)
-
-    def __init__(self, user_id, afk_time, reason="", is_afk=True):
-        self.user_id = user_id
-        self.afk_time = afk_time
-        self.reason = reason
-        self.is_afk = is_afk
-
-    def __repr__(self):
-        return "afk_status for {}".format(self.user_id)
+AFK_GROUP = 7
+AFK_REPLY_GROUP = 8
 
 
-AFK.__table__.create(checkfirst=True)
-INSERTION_LOCK = threading.RLock()
+@run_async
+def afk(update: Update, context: CallbackContext):
+    args = update.effective_message.text.split(None, 1)
+    afk_time = int(time.time())
+    notice = ""
+    if len(args) >= 2:
+        reason = args[1]
+        if len(reason) > 100:
+            reason = reason[:100]
+            notice = "\nYour afk reason was shortened to 100 characters."
+    else:
+        reason = ""
 
-AFK_USERS = {}
-
-
-def is_afk(user_id):
-    return user_id in AFK_USERS
-
-
-def check_afk_status(user_id):
-    try:
-        return SESSION.query(AFK).get(user_id)
-    finally:
-        SESSION.close()
-
-
-def get_afk_time(user_id):
-    afktime = SESSION.query(AFK).get(user_id)
-    SESSION.close()
-    if afktime:
-        return afktime.afk_time
-    return None
+    sql.set_afk(update.effective_user.id, afk_time, reason)
+    fname = update.effective_user.first_name
+    update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
 
 
-def set_afk(user_id, afk_time, reason=""):
-    with INSERTION_LOCK:
-        curr = SESSION.query(AFK).get(user_id)
-        if not curr:
-            curr = AFK(user_id, afk_time, reason, True)
+@run_async
+def no_longer_afk(update: Update, context: CallbackContext):
+    user = update.effective_user
+    message = update.effective_message
+
+    if not user:  # ignore channels
+        return
+
+    res = sql.rm_afk(user.id)
+    if res:
+        if message.new_chat_members:  # dont say msg
+            return
+        firstname = update.effective_user.first_name
+        try:
+            options = [
+                "{} is here!",
+                "{} is back!",
+                "{} is now in the chat!",
+                "{} is awake!",
+                "{} is back online!",
+                "{} is finally here!",
+                "Welcome back! {}",
+                "Where is {}?\nIn the chat!",
+            ]
+            chosen_option = random.choice(options)
+            update.effective_message.reply_text(chosen_option.format(firstname))
+        except:
+            return
+
+
+@run_async
+def reply_afk(update: Update, context: CallbackContext):
+    bot = context.bot
+    message = update.effective_message
+    userc = update.effective_user
+    userc_id = userc.id
+    if message.entities and message.parse_entities(
+        [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+    ):
+        entities = message.parse_entities(
+            [MessageEntity.TEXT_MENTION, MessageEntity.MENTION]
+        )
+
+        chk_users = []
+        for ent in entities:
+            if ent.type == MessageEntity.TEXT_MENTION:
+                user_id = ent.user.id
+                fst_name = ent.user.first_name
+
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
+
+            if ent.type == MessageEntity.MENTION:
+                user_id = get_user_id(
+                    message.text[ent.offset : ent.offset + ent.length]
+                )
+                if not user_id:
+                    # Should never happen, since for a user to become AFK they must have spoken. Maybe changed username?
+                    return
+
+                if user_id in chk_users:
+                    return
+                chk_users.append(user_id)
+
+                try:
+                    chat = bot.get_chat(user_id)
+                except BadRequest:
+                    print(
+                        "Error: Could not fetch userid {} for AFK module".format(
+                            user_id
+                        )
+                    )
+                    return
+                fst_name = chat.first_name
+
+            else:
+                return
+
+            check_afk(update, context, user_id, fst_name, userc_id)
+
+    elif message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        fst_name = message.reply_to_message.from_user.first_name
+        check_afk(update, context, user_id, fst_name, userc_id)
+
+
+def check_afk(update, context, user_id, fst_name, userc_id):
+    if sql.is_afk(user_id):
+        user = sql.check_afk_status(user_id)
+        afk_time = sql.get_afk_time(user_id)
+        afk_since = get_readable_time((time.time() - afk_time))
+        if not user.reason:
+            if int(userc_id) == int(user_id):
+                return
+            res = "{} is afk since {}".format(fst_name, afk_since)
+            update.effective_message.reply_text(res)
         else:
-            curr.is_afk = True
-
-        AFK_USERS[user_id] = reason
-
-        SESSION.add(curr)
-        SESSION.commit()
-
-
-def rm_afk(user_id):
-    with INSERTION_LOCK:
-        curr = SESSION.query(AFK).get(user_id)
-        if curr:
-            if user_id in AFK_USERS:  # sanity check
-                del AFK_USERS[user_id]
-
-            SESSION.delete(curr)
-            SESSION.commit()
-            return True
-
-        SESSION.close()
-        return False
+            if int(userc_id) == int(user_id):
+                return
+            res = "{} is afk since {}\nReason: {}".format(
+                fst_name, afk_since, user.reason
+            )
+            update.effective_message.reply_text(res)
 
 
-def toggle_afk(user_id, reason=""):
-    with INSERTION_LOCK:
-        curr = SESSION.query(AFK).get(user_id)
-        if not curr:
-            curr = AFK(user_id, reason, True)
-        elif curr.is_afk:
-            curr.is_afk = False
-        elif not curr.is_afk:
-            curr.is_afk = True
-        SESSION.add(curr)
-        SESSION.commit()
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
 
 
-def __load_afk_users():
-    global AFK_USERS
-    try:
-        all_afk = SESSION.query(AFK).all()
-        AFK_USERS = {user.user_id: user.reason for user in all_afk if user.is_afk}
-    finally:
-        SESSION.close()
+__help__ = """
+ • `/afk <reason>`*:* mark yourself as AFK(away from keyboard).
+ • `brb <reason>`*:* same as the afk command - but not a command.
+When marked as AFK, any mentions will be replied to with a message to say you're not available!
+"""
 
+AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
+AFK_REGEX_HANDLER = MessageHandler(Filters.regex(r"(?i)brb"), afk, friendly="afk")
+NO_AFK_HANDLER = MessageHandler(Filters.all & Filters.group, no_longer_afk)
+AFK_REPLY_HANDLER = MessageHandler(Filters.all & Filters.group, reply_afk)
 
-__load_afk_users()
+dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
+dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
+dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
+
+__mod_name__ = "AFK"
+__command_list__ = ["afk"]
+__handlers__ = [
+    (AFK_HANDLER, AFK_GROUP),
+    (AFK_REGEX_HANDLER, AFK_GROUP),
+    (NO_AFK_HANDLER, AFK_GROUP),
+    (AFK_REPLY_HANDLER, AFK_REPLY_GROUP),
+]
